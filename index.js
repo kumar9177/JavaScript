585adb8a3111dcf64b7b4ce83ad1fbf1e6e7072a
mekalavenkatesh <mekalavenky@example.com>
2024-01-02 18:46:37 +0530

    JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello kumar')
+// const age=22
+// const name ="kumar"
+// console.log(typeof name ,typeof age)
+// const array =["name1","name2","name3"]
+// console.log(typeof array)
+// console.log(name.length,age.length)
+// function  greet(){
+//    const  name="kumar"
+// }
+
+const myObj = {
+    name: "kumar",
+    age:22,
+    address: "mallapur",
+cae:{
+    color:"red",
+    number:123,
+    type:"sports",
+}}
+    console.log(myObj)
+    function greet(time){
+        console.log("good morning"+time)
+
+
+    }
+ greet()
+    
+const val ="12" + 1
+console.log(val)
